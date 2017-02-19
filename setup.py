6ba67b29fcff3796fe7cde5e248f4f5857430270
Nitish Reddy Koripalli <nitish.k.reddy@gmail.com>
2017-02-19 17:59:36 +0100

    name change

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 """Project setup file."""
 from setuptools import find_packages, setup
 
-setup(name='quick_anno',
+setup(name='decoratory',
       version='0.0.1',
       description='Quick and simple annotations for Python scripts.',
       author='Nitish Reddy Koripalli',
       author_email='nitish.k.reddy@gmail.com',
-      url='https://github.com/nitred/quick_anno',
-      download_url='https://github.com/nitred/quick_anno/master.tar.gz',
+      url='https://github.com/nitred/decoratory',
+      download_url='https://github.com/nitred/decoratory/master.tar.gz',
       license='MIT',
       install_requires=['future', 'requests', 'requests-futures'],
       packages=find_packages())
