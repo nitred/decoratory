1587a941f96c062ca2b8a3d1cb78fee7c7240363
Ankur Srivastava <ankur.srivastava@recogizer.de>
2017-09-29 11:38:07 +0200

    unwanted requests module

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import find_packages, setup
 
 setup(name='decoratory',
-      version='0.0.3',
+      version='0.1.0',
       description='Quick and simple annotations for Python scripts.',
       author='Nitish Reddy Koripalli',
       author_email='nitish.k.reddy@gmail.com',
       url='https://github.com/nitred/decoratory',
       download_url='https://github.com/nitred/decoratory/master.tar.gz',
       license='MIT',
-      install_requires=['future', 'requests', 'requests-futures'],
+      install_requires=['future'],
       packages=find_packages())
